bd7a983325a3280b65fc1f342b95d139ab71a89e
Uno Johansson <swox.player@gmail.com>
2023-01-08 13:05:04 +0100

    a

--- translator/c194469bd2536ce992da600cd49ba99e7cd98179/index.js
+++ translator/c194469bd2536ce992da600cd49ba99e7cd98179/index.js
@@ -1,13 +1,6 @@
 let english = ['A', 'About', 'Alert', 'Am', 'Atleast', 'Authorities', 'Avoid', 'Building', 'Done', 'Ever', 'Government', 'In', 'Know', 'Me', 'Mine', 'More', 'Must', 'Never', 'Our', 'Other', 'Said', 'Say', 'Skeebonia', 'So', 'Swedish', 'To', 'That', 'Than', 'Them', 'They', 'Those', 'Though', 'Thorough', 'Through', 'Under', 'Understand', 'Understanding', 'Understatement', 'Very', 'Where', 'Weaponry', 'You', 'Your']
 let skeebonian = ['I', 'Jafer', 'Fæðür', 'Zor', 'Tarrik', 'Chenðæneri', 'Hürent', 'Ünterzal', 'Keð', 'Tsanchi', 'Gzentelmoð', 'Oer', 'Mben', 'Mü', 'Müs', 'Ið', 'Bœz', 'Mvender', 'Iztr', 'Nteher', 'Zþan', 'Lætr', 'Skeebonia', 'Læ', 'Sedðes', 'La', 'Ghe', 'Zheþ', 'Đen', 'Đei', 'Gæs', 'Tol', 'Tarnzich', 'Tier', 'Gzech', 'Ürðets', 'Ürðetsin', 'Æþünfrich', 'Tgare', 'Üchzal', 'Eðesvarn', 'Te', 'Tes']
 
-function onload() {
-    for (let i = 0; i < english.length; i++) {
-        english[i] = english[i].toLowerCase()
-        skeebonian[i] = skeebonian[i].toLowerCase()
-    }
-
-
 function btnclicked() {
     let inputfield = document.getElementById('english')
     let input = inputfield.value.toLowerCase()
@@ -29,6 +22,15 @@ function btnclicked() {
     outputText.innerText = output
 
 }
+
+function onload() {
+    for (let i = 0; i < english.length; i++) {
+        english[i] = english[i].toLowerCase()
+        skeebonian[i] = skeebonian[i].toLowerCase()
+    }
+
+
+
 document.getElementById("trbtn").onclick = btnclicked
 }
 
